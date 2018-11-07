159553db6a8c0f93557904840354f52b828b3f7b
qmch <aura1337@gmail.com>
2018-11-07 16:14:01 +0000

    PDFs updated! See last commit message(s) for details.

---
+++ autopdf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Beginning PDF-making!"
+#TeX the GR notes and copy them to the PDFs folder
+cd ./michaelmas/gr
+latexmk -pdf gr-notes-partiii.tex
+latexmk -c
+cp gr-notes-partiii.pdf ../../pdfs
+
+#same thing for QFT
+cd ../qft
+latexmk -pdf qft-notes-partiii.tex
+latexmk -c
+cp qft-notes-partiii.pdf ../../pdfs
+
+#and for Symmetries!
+cd ../sfp
+latexmk -pdf sfp-notes-partiii.tex
+latexmk -c
+cp sfp-notes-partiii.pdf ../../pdfs
+
+echo "PDFs made. Pushing to the remote."
+#finally, return to the home directory, git add, and git commit.
+cd ../.. #to the main directory
+git add . #add all the updated files
+git commit -m "PDFs updated! See last commit message(s) for details."
+git push
+echo "autopdf complete."
